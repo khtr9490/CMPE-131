f94dff9e26a5b95c2e5f1ff961e881d7e632f26e
greg <gregory.vansciver@sjsu.edu>
2020-04-16 20:43:16 -0700

    home page files

---
+++ frontend/index.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>Online Food Store</title>
+        <link rel="stylesheet" href="styles.css"></link>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet"> 
+    </head>
+    <body>
+        
+        <div id="header"> 
+            <div class="headerleft">
+                <h1>Online Food Store</h1>
+            </div>
+            <div class="headerright">
+                <h2>Log in</h2>
+            </div>
+        </div>
+
+    </body>
+</html>
