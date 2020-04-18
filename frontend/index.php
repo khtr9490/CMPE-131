268f613463f95e9a1078aace332a2f1391a55546
jeffery vong <jefferyvong@jefferys-MacBook-Pro.local>
2020-04-17 23:16:58 -0700

    Changed the home pageand add all the layouts. Rough draft for now.

--- frontend/index.php
+++ frontend/index.php
@@ -1,19 +1,40 @@
+<!DOCTYPE html>
 <html>
-    <head>
-        <title>Online Food Store</title>
-        <link rel="stylesheet" href="styles.css"></link>
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet"> 
-    </head>
-    <body>
-        
-        <div id="header"> 
-            <div class="headerleft">
-                <h1>Online Food Store</h1>
-            </div>
-            <div class="headerright">
-                <h2>Log in</h2>
-            </div>
-        </div>
+<head>
+  <title>Online Food Store</title>
+  <link rel="stylesheet" href="styles.css">
+<head>
+<body>
+<header>
+    <nav>
+      <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="catalog.html">Catalog</a></li>
+        <li class="logo"><a href="index.php">Online Food Store Logo</a></li>
+        <li><a href="login.html">Login/Register</a></li>
+        <li><a href="cart.html">Cart</a></li>
+      </ul>
+    </nav>
+  </header>
+  <section class="features">
+    <figure>
+        <img src="/img/Features/AllCategories.jpg" alt="Categories">
+        <figcaption>All Categories</figcaption>
+      </figure>
+      <figure>
+          <img src="/img/Features/Product.jpg" alt="Product">
+          <figcaption>Product</figcaption>
+        </figure>
+        <figure>
+            <img src="/img/Features/Profile.jpg" alt="Profile">
+            <figcaption>Profile</figcaption>
+          </figure>
+  </section>
 
-    </body>
+  <footer>
+    123 Main Street San Jose, CA • 408-111-2345 • OnlineFoodStore@me.com
+  </footer>
+
+</body>
+
 </html>
