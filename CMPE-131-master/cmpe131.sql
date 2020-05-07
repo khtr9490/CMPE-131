8bfc44c9bb35f6a1eeefa3ba5696076b7b89b294
khtr9490 <61854598+khtr9490@users.noreply.github.com>
2020-05-07 12:37:29 -0700

    Add files via upload

--- CMPE-131-master/cmpe131.sql
+++ CMPE-131-master/cmpe131.sql
@@ -66,6 +66,20 @@ CREATE TABLE `brands` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `id` int(10) NOT NULL,
+  `p_id` int(10) NOT NULL,
+  `ip_add` varchar(250) NOT NULL,
+  `user_id` int(10) DEFAULT NULL,
+  `qty` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `categories`
 --
 
@@ -73,6 +87,100 @@ CREATE TABLE `categories` (
   `cat_id` int(100) NOT NULL,
   `cat_title` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`cat_id`, `cat_title`) VALUES
+(1, 'Dairy'),
+(2, 'Fruits'),
+(3, 'Meat'),
+(4, 'Vegetables');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `trx_id` varchar(255) NOT NULL,
+  `p_status` varchar(20) NOT NULL,
+  `o_id` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`order_id`, `user_id`, `product_id`, `qty`, `trx_id`, `p_status`, `o_id`) VALUES
+(2, 25, 2, 1, '', 'pending', 8),
+(5, 25, 2, 1, '', 'pending', 9),
+(6, 25, 2, 1, '', 'pending', 10),
+(2, 1, 10, 1, '', 'pending', 11),
+(3, 1, 9, 1, '', 'pending', 12);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders_info`
+--
+
+CREATE TABLE `orders_info` (
+  `order_id` int(10) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `f_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `state` varchar(255) NOT NULL,
+  `zip` int(10) NOT NULL,
+  `cardname` varchar(255) NOT NULL,
+  `cardnumber` varchar(20) NOT NULL,
+  `expdate` varchar(255) NOT NULL,
+  `prod_count` int(15) DEFAULT NULL,
+  `total_amt` decimal(10,2) DEFAULT NULL,
+  `cvv` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `orders_info`
+--
+
+INSERT INTO `orders_info` (`order_id`, `user_id`, `f_name`, `email`, `address`, `city`, `state`, `zip`, `cardname`, `cardnumber`, `expdate`, `prod_count`, `total_amt`, `cvv`) VALUES
+(1, 1, 'Khoi Tran', 'abc@gmail.com', '123 abc, san jose CA 95111', 'SAN JOSE', 'CA', 95148, 'Steven Massenburg', '4318 8392 7914 5007', '05/22', 3, '16.00', 996),
+(2, 1, 'Khoi Tran', 'abc@gmail.com', '123 abc, san jose CA 95111', 'SAN JOSE', 'CA', 95148, 'Steven Massenburg', '4318 8392 7914 5007', '05/22', 3, '4.97', 996),
+(3, 1, 'Khoi Tran', 'abc@gmail.com', '123 abc, san jose CA 95111', 'SAN JOSE', 'CA', 95148, 'Steven Massenburg', '4318 8392 7914 5007', '05/22', 1, '6.99', 996);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_products`
+--
+
+CREATE TABLE `order_products` (
+  `order_pro_id` int(10) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `qty` int(15) DEFAULT NULL,
+  `amt` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `order_products`
+--
+
+INSERT INTO `order_products` (`order_pro_id`, `order_id`, `product_id`, `qty`, `amt`) VALUES
+(98, 1, 8, 1, '5.00'),
+(99, 1, 9, 1, '6.00'),
+(100, 1, 10, 1, '2.00'),
+(101, 2, 10, 1, '2.00'),
+(102, 3, 9, 1, '6.00');
 
 -- --------------------------------------------------------
 
@@ -85,11 +193,25 @@ CREATE TABLE `products` (
   `product_cat` varchar(100) NOT NULL,
   `product_brand` varchar(100) NOT NULL,
   `product_title` varchar(255) NOT NULL,
-  `product_price` int(100) NOT NULL,
+  `product_price` decimal(10,2) NOT NULL,
   `product_desc` text NOT NULL,
   `product_image` text NOT NULL,
-  `product_keywords` text NOT NULL
+  `product_keywords` text NOT NULL,
+  `weight` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`product_id`, `product_cat`, `product_brand`, `product_title`, `product_price`, `product_desc`, `product_image`, `product_keywords`, `weight`) VALUES
+(8, '1', 'Silk', 'Almond Milk', '5.99', 'Almond Milk', '1588859891_AlmondMilk.jpg', 'Almond Milk', '0.00'),
+(9, '1', 'Singles', 'American Cheese', '6.99', 'American Cheese', '1588859945_AmericanCheese.jpg', 'Cheese', '0.00'),
+(10, '1', 'None', 'Eggs', '2.99', 'Eggs', '1588860087_Eggs.jpg', 'egg', '0.00'),
+(11, '2', 'Fuji', 'Apple', '0.99', 'Apple', '1588860134_Apple.jpg', 'apple', '0.00'),
+(12, '2', 'None', 'Banana', '0.99', 'Banana', '1588860163_Banana.jpg', 'banana', '0.00'),
+(13, '3', 'ham', 'Ham', '3.99', 'Ham', '1588860202_Ham.jpg', 'ham', '0.00'),
+(14, '3', 'Cow', 'Steak', '6.99', 'Steak', '1588860248_Steak.jpg', 'steak', '0.00');
 
 -- --------------------------------------------------------
 
@@ -110,6 +232,15 @@ CREATE TABLE `user_info` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Dumping data for table `user_info`
+--
+
+INSERT INTO `user_info` (`user_id`, `user_name`, `first_name`, `last_name`, `email`, `password`, `mobile`, `address1`, `address2`) VALUES
+(1, 'koi', 'Khoi', 'Tran', 'abc@gmail.com', '1234', '4027721111', '123 abc, san jose CA 95111', ''),
+(2, 'mummy3099', 'khoi', 'tran', 'asv@gmail.com', 'DestinyGundam0123', '4087775555', '123 san jose', 'san jose'),
+(3, 'abc', 'asd', 'asd', 'avc@gmail.com', 'Koi0908358424', '4087775555', '123 san jose', 'san jose');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -126,10 +257,37 @@ ALTER TABLE `brands`
   ADD PRIMARY KEY (`brand_id`);
 
 --
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `categories`
 --
 ALTER TABLE `categories`
   ADD PRIMARY KEY (`cat_id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`o_id`);
+
+--
+-- Indexes for table `orders_info`
+--
+ALTER TABLE `orders_info`
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `order_products`
+--
+ALTER TABLE `order_products`
+  ADD PRIMARY KEY (`order_pro_id`),
+  ADD KEY `order_products` (`order_id`),
+  ADD KEY `product_id` (`product_id`);
 
 --
 -- Indexes for table `products`
@@ -160,22 +318,40 @@ ALTER TABLE `brands`
   MODIFY `brand_id` int(100) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=171;
+
+--
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `cat_id` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `cat_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `o_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `order_products`
+--
+ALTER TABLE `order_products`
+  MODIFY `order_pro_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=103;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `product_id` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `product_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `user_info`
 --
 ALTER TABLE `user_info`
-  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
